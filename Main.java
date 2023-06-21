530ea7a75ee9e9be174c5542a97fcded0ef2d72a
Sasha <sashok.ey@mail.ru>
2023-06-21 19:01:25 +0800

    asdasqwd

--- Main.java
+++ Main.java
@@ -1,64 +1,77 @@
 package education;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+// Даны два Deque, представляющие два целых числа.
+// Цифры хранятся в обратном порядке и каждый из их узлов содержит одну цифру.
+//1) Умножьте два числа и верните произведение в виде связанного списка.
+//2) Сложите два числа и верните сумму в виде связанного списка.
+//Одно или два числа могут быть отрицательными.
+//
+//Даны два Deque, цифры в обратном порядке.
+//[3,2,1,-] - пример Deque
+//[5,4,3]- пример второго Deque
+//1) -123 * 345 = -42 435
+//Ответ всегда - связный список, в обычном порядке
+//[-,4,2,4,3,5] - пример ответа
 
-// Пусть дан произвольный список целых чисел.
-//1) Нужно удалить из него чётные числа
-//2) Найти минимальное значение
-//3) Найти максимальное значение
-//4) Найти среднее значение
 // Шугалей Александр
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> spisok = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            spisok.add(random.nextInt(10));
-        }
-        System.out.println(spisok);
+        ArrayDeque<String> dq1 = new ArrayDeque<>(Arrays.asList("3", "2", "-"));
+        ArrayDeque<String> dq2 = new ArrayDeque<>(Arrays.asList("5", "4", "3"));
+        System.out.println(dq1);
+        System.out.println(dq2);
     }
 
-    public static void task1(List<Integer> spisok) {
-        for (int i = 0; i < spisok.size(); i++) {
-            if (spisok.get(i) % 2 == 0) {
-                spisok.remove(i);
-                i--;
-            }
+    public static LinkedList<Character> task1(ArrayDeque<String> dq1, ArrayDeque<String> dq2){
+        LinkedList<Character> ll = new LinkedList<>();
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < dq1.size(); i++) {
+            sb1.append(dq1.pollLast());
         }
+        sb1.append(dq1.pop());
+        int a = Integer.parseInt(sb1.toString());
+        for (int i = 0; i < dq2.size(); i++) {
+            sb2.append(dq2.pollLast());
+        }
+        sb2.append(dq2.pop());
+        int b = Integer.parseInt(sb2.toString());
+        int res = a * b;
+        String ss = Integer.toString(res);
+        sb.append(ss);
+        for (int i = 0; i < sb.length(); i++) {
+            ll.add(sb.charAt(i));
+        }
+        return ll;
     }
 
-    public static int task2(List<Integer> spisok) {
-        int min = 1000000;
-        for (int i = 0; i < spisok.size(); i++) {
-            if (spisok.get(i) < min) {
-                min = spisok.get(i);
-            }
+    public static LinkedList<Character> task2(ArrayDeque<String> dq1, ArrayDeque<String> dq2){
+        LinkedList<Character> ll = new LinkedList<>();
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < dq1.size(); i++) {
+            sb1.append(dq1.pollLast());
         }
-        return min;
-    }
-
-    public static int task3(List<Integer> spisok) {
-        int max = 0;
-        for (int i = 0; i < spisok.size(); i++) {
-            if (spisok.get(i) > max) {
-                max = spisok.get(i);
-            }
+        sb1.append(dq1.pop());
+        int a = Integer.parseInt(sb1.toString());
+        for (int i = 0; i < dq2.size(); i++) {
+            sb2.append(dq2.pollLast());
         }
-        return max;
-    }
-
-    public static double task4(List<Integer> spisok){
-        double sum = 0;
-        double mid = 0;
-        double len = spisok.size();
-        for (int i = 0; i < spisok.size(); i++) {
-            sum = sum + spisok.get(i);
+        sb2.append(dq2.pop());
+        int b = Integer.parseInt(sb2.toString());
+        int res = a + b;
+        String ss = Integer.toString(res);
+        sb.append(ss);
+        for (int i = 0; i < sb.length(); i++) {
+            ll.add(sb.charAt(i));
         }
-        mid = sum / spisok.size();
-        return mid;
+        return ll;
     }
-
 }
